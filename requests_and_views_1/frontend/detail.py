196dde6c33ec984936645d8e6e4cf30dcce6d87d
elyordusnazarov <elyor.dusnazarov@gmail.com>
2023-05-28 21:15:32 +0500

    requests_and_views_1

---
+++ requests_and_views_1/frontend/detail.py
@@ -0,0 +1,9 @@
+
+# ////////////////  DRF Generics RetrieveAPIView    ///////////////////////
+import requests
+
+endpoint = "http://127.0.0.1:8000/api/products/100/"
+
+get_response = requests.get(endpoint)
+print(get_response.json())
+
