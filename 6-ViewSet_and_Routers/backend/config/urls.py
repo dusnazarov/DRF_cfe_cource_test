3b356d7e5223a58305a835468247fd5929cdc724
elyordusnazarov <elyor.dusnazarov@gmail.com>
2023-06-01 15:17:24 +0500

    6-ViewSet_and_Routers

---
+++ 6-ViewSet_and_Routers/backend/config/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path, include 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+    path('api/products/', include('products.urls')),
+    path('api/v2/', include('config.routers')),
+]
+
+
+
